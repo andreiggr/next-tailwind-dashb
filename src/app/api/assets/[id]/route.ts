e04c4e2722c77ba8683ce8068b0344ecaed32bcc
Andrei Grigore <andrei.ggr@gmail.com>
2025-04-19 14:51:05 +0300

    fix api route

--- src/app/api/assets/[id]/route.ts
+++ src/app/api/assets/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { assets } from "../../mockData";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: any) {
   const { id } = await params;
   const asset = assets.find((a) => a.id === id);
   if (!asset) {
